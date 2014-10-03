5dcc1af707f3fc77dfba3d15da544fa391ec3a96
Jaak Simm <jaak.simm@gmail.com>
2014-10-03 02:34:48 +0200

    test for extrema

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,9 @@
 using ExtraTrees
 using Base.Test
 
-# write your own tests here
-@test 1 == 1
+# testing extrema
+x = [0.1 0.2; 0.5 0.9; -0.1 1.1]
+@test ExtraTrees.extrema(x, [1,2,3], 1) == (-0.1, 0.5)
+@test ExtraTrees.extrema(x, [1,2,3], 2) == ( 0.2, 1.1)
+@test ExtraTrees.extrema(x, [2,1],   1) == ( 0.1, 0.5)
+
